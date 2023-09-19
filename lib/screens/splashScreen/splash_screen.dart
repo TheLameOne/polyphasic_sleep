5c95348b669e756f2f74aac3aa002552fde9fec0
Harsh Verma <vermaharsh463@gmail.com>
2023-09-20 01:34:16 +0530

    both Splash Page completed

---
+++ lib/screens/splashScreen/splash_screen.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:polyphasic_sleep/TestScreen.dart';
+import 'package:polyphasic_sleep/screens/splashScreen/splash_screen2.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const String routeNamed = "SplashScreen";
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SplashScreen2(),
+          ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.amber[50],
+      body: Container(
+          child: Center(
+              child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(image: AssetImage("assets/images/polylogo.png")),
+          SizedBox(height: 32),
+          Text(
+            "POLYPHASIC SLEEP",
+            style: TextStyle(
+                fontSize: 38,
+                fontWeight: FontWeight.bold,
+                color: Colors.lightBlue),
+          )
+        ],
+      ))),
+    );
+  }
+}
---
+++ lib/screens/splashScreen/splash_screen2.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:polyphasic_sleep/TestScreen.dart';
+
+class SplashScreen2 extends StatefulWidget {
+  static const String routeNamed = "SplashScreen2";
+
+  @override
+  State<SplashScreen2> createState() => _SplashScreen2State();
+}
+
+class _SplashScreen2State extends State<SplashScreen2> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 1), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => TestScreen(),
+          ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.amber[50],
+      body: Container(
+          child: Center(
+              child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          "Reach your full potential by sleeping more than once each day.",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[600]),
+        ),
+      ))),
+    );
+  }
+}
